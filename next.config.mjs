0232c310fea94c5f93986b51a4522ce50074e880
planmybaraat-droid <planmybaraat@gmail.com>
2026-08-17 13:55:03 +0530

    Correct next.config.mjs replacement

--- next.config.mjs
+++ next.config.mjs
@@ -43,27 +43,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/icons/icon.svg",
-        destination: "/icon-mark-180.png",
-        permanent: true,
-      },
-    ];
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/photo-**",
-      },
-    ],
-  },
-};
-
-export default nextConfig;
