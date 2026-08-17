a61f170a27f0f8808319ea85bd8c9882043da94a
planmybaraat-droid <planmybaraat@gmail.com>
2026-08-17 13:58:31 +0530

    Correct app/robots.ts replacement

--- app/robots.ts
+++ app/robots.ts
@@ -21,17 +21,3 @@ export default function robots(): MetadataRoute.Robots {
     sitemap: "https://planmybaraat.com/sitemap.xml",
   };
 }
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/crm", "/crm/", "/admin", "/admin/", "/marketplace", "/marketplace/"],
-      },
-    ],
-    sitemap: "https://planmybaraat.com/sitemap.xml",
-  };
-}
